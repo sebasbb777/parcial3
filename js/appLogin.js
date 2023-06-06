3523f58feb987c3edb75229477d06f6412e64259
yiis <giselay.09@gmail.com>
2023-06-06 10:15:33 -0600

    PORFIIIIN!

--- js/appLogin.js
+++ js/appLogin.js
@@ -4,18 +4,47 @@ const registrarUsuario=()=>{
     var correo=document.querySelector("#correo").value;
     var password=document.querySelector("#password").value;
     var nombre=document.querySelector("#nombre").value;
-
+    console.log("Password: "+password)
     if(correo.trim()===''||
     password.trim()===''||
     nombre.trim()===''){
         Swal.fire({
             icon: 'error',
             title: 'ERROR',
-            text: 'FALTA LLENAR CAMPO',
+            text: 'FALTA UN CAMPO POR LLENAR',
             footer: 'CRUD CONTACTOS'
           })
 
     }
+    
+    if(!validarCorreo(correo)){
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'INTRODUCE UN CORREO ELECTRONICO VALIDO',
+            footer: 'CRUD CONTACTOS'
+        })
+        return;
+    }
 
+    if(!validarPassword(password)){
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            html: 'INTRODUCE UN PASSWORD VÁLIDO <br> [Mayúsculas, minúsculas, números y mín. 8 Carácteres]',
+            footer: 'CRUD CONTACTOS'
+        })
+        return;
+    }
+
+    if(!validarNombre(nombre)){
+        Swal.fire({
+            icon: 'error',
+            title: 'ERROR',
+            text: 'INTRODUCE UN NOMBRE VALIDO',
+            footer: 'CRUD CONTACTOS'
+        })
+        return;
+    }
 
 }
